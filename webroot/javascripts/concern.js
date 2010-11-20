73fb04fd3ff85681e06dfc4be27ed5f0178e0bcd
Gigix <user@ThoughtWorkss-MacBook-Pro.local>
2010-11-20 21:10:00 +0800

    create and load concerns

--- webroot/javascripts/concern.js
+++ webroot/javascripts/concern.js
@@ -1,6 +1,10 @@
-function Concern() {
+function Concern(title) {
 	this.id = -1
 	this.inception_id = -1
-	this.title = ''
+	this.title = title
 	this.checkpoints = new Array()
+}
+
+Concern.prototype.create = function(tx, inception_id) {
+	tx.executeSql("INSERT INTO concerns (inception_id, title) VALUES (?, ?)", [inception_id, this.title], null, null)
 }
--- webroot/javascripts/inception.js
+++ webroot/javascripts/inception.js
@@ -1,18 +1,51 @@
-function Inception() {
+function Inception(title) {
 	this.id = -1
-	this.title = ''
+	this.title = title
 	this.concerns = new Array()
 }
 
-function createInceptionWithinTransaction(tx, title) {
-	tx.executeSql("INSERT INTO inceptions (title) VALUES (?)", [title], null, null)			
+Inception.prototype.loadConcerns = function(tx, handle) {
+	var inception = this
+	tx.executeSql("SELECT * FROM concerns WHERE inception_id = ? ORDER BY id ASC", [inception.id],
+		function(tx, result) {
+			for(var i = 0; i < result.rows.length; i++) {
+				var record = result.rows.item(i)
+				
+				var concern = new Concern()
+				concern.id = record['id']
+				concern.inception_id = record['inception_id']
+				concern.title = record['title']
+				
+				inception.concerns.push(concern)
+			}
+			handle(inception)
+		},
+		null
+	)
+}
+
+Inception.prototype.create = function(tx) {
+	tx.executeSql("INSERT INTO inceptions (title) VALUES (?)", [this.title], null, null)			
+
+	var concerns = this.concerns
 	tx.executeSql("SELECT id FROM inceptions ORDER BY id DESC", [], 
 		function(tx, result) {
 			var inception_id = result.rows.item(0)['id']
-			tx.executeSql("INSERT INTO concerns (inception_id, title) VALUES (?, ?)", [inception_id, "Integration"], null, null)
+			for(var i = 0; i < concerns.length; i++) {
+				var concern = concerns[i]
+				concern.create(tx, inception_id)
+			}
 		}, 
 		null
 	)
+}
+
+function createInception(title) {
+	var inception = new Inception(title)
+	inception.concerns.push(new Concern('Integration'))
+	inception.concerns.push(new Concern('Data Migration'))
+	
+	db.transaction(function(tx) {inception.create(tx) })
 }
 
 function listInceptionsWithinTransaction(tx, handleEach) {
@@ -20,22 +53,16 @@ function listInceptionsWithinTransaction(tx, handleEach) {
 		function(tx, result) {
 			var inceptions = new Array()
 			for(var i = 0; i < result.rows.length; i++) {
-				var inception = new Inception()
 				var record = result.rows.item(i)
+				
+				var inception = new Inception()
 				inception.id = record['id']
 				inception.title = record['title']
-				handleEach(inception)
+				
+				inception.loadConcerns(tx, handleEach)
 			}
 		},
 		null
-	)
-}
-
-function createInception(title) {
-	db.transaction(
-		function(tx) {
-			createInceptionWithinTransaction(tx, title)
-		}
 	)
 }
 
