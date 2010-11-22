a50728813b47a540cecd62a6704e7fb9be334565
Gigix <user@ThoughtWorkss-MacBook-Pro.local>
2010-11-22 21:45:26 +0800

    index.html: initialize DB if necessary

--- webroot/javascripts/db_utils.js
+++ webroot/javascripts/db_utils.js
@@ -1,14 +1,11 @@
 var db = openDatabase("Jungling", "0.1", "Jungling", 200000)
 
 function initializeDatabase() {	
-	db.transaction(
-		function(tx) {
-			tx.executeSql("DROP TABLE inceptions", [], null, null)
-			tx.executeSql("DROP TABLE concerns", [], null, null)
-			tx.executeSql("DROP TABLE checkpoints", [], null, null)
-		}
-	)
-	
+	dropTables()
+	createTables()
+}
+
+function createTables() {
 	db.transaction(
 		function(tx) {
 			tx.executeSql("CREATE TABLE inceptions (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT)", [], null, null)
@@ -16,4 +13,27 @@ function initializeDatabase() {
 			tx.executeSql("CREATE TABLE checkpoints (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, concern_id INTEGER NOT NULL, title TEXT, description TEXT, comment TEXT, status TEXT)", [], null, null)
 		}
 	)	
+}
+
+function dropTables() {
+	db.transaction(
+		function(tx) {
+			tx.executeSql("DROP TABLE inceptions", [], null, null)
+			tx.executeSql("DROP TABLE concerns", [], null, null)
+			tx.executeSql("DROP TABLE checkpoints", [], null, null)
+		}
+	)
+}
+
+function verifyDatabase(validHandler, invalidHandler) {
+	db.transaction(function(tx) {
+		tx.executeSql("SELECT 1 FROM inceptions", [], 
+			function(tx, result) {
+				validHandler()
+			},
+			function(tx, error) {
+				invalidHandler()
+			}
+		)
+	})
 }
